5556c21ae19a843ce778123595f8aa2c4dfd8ed5
Anjana-20782 <anjanasivadasan0305@gmail.com>
2025-12-03 12:39:24 +0530

    script.js file and contents   added

--- public/script.js
+++ public/script.js
@@ -1,7 +1,31 @@
-const display = document.getElementById('display');
+const list= document.getElementById('display');
 
+fetchTodos()
+function fetchTodos(){
+    fetch("/api/todos")
+    .then(res=>res.json())
+    .then(data =>{
+         list.innerHTML = "";
+         data.forEach(todo => {
+            innerHTML+=`
+            ${todo.task}
+            `
+         });
+    })
+}
 function addTodo()
 {
-    
+   const input=document.getElementById("todo-input") 
+   const task=input.ariaValueMax;
+
+   fetch("/api/todos",{
+    method:"POST",
+    headers:{"Content-Type":"application/json"},
+    body:JSON.stringify({task})
+   })
+    .then(()=>{
+        task = ""
+        fetchTodos()
+    });
 }
   
