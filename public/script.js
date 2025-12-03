7961d040e421f3cbc5d4769396065ee52235fdcf
Anjana-20782 <anjanasivadasan0305@gmail.com>
2025-12-03 10:24:32 +0530

    js file and contents   added

---
+++ public/script.js
@@ -0,0 +1,13 @@
+const addBtn = document.getElementById('addBtn');
+const todoInput = document.getElementById('todo-input');
+
+addBtn.addEventListener('click', () => {
+    const text = todoInput.value;
+
+    if(text === '') {
+        alert('Please enter a todo item');
+        return;
+    }
+    console.log(text);
+})
+    
