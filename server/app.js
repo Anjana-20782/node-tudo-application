1600d1d526bb52f07cab6877239ccdf2259c1986
Anjana-20782 <anjanasivadasan0305@gmail.com>
2025-12-07 15:11:31 +0530

    new contents added in the node-tudo-application

--- server/app.js
+++ server/app.js
@@ -1,14 +1,28 @@
-const fs=require('fs');
-const http=require('http');
-const path=require('path');
-const router=require('./router');
+// const fs=require('fs');
+// const http=require('http');
+// const path=require('path');
+// const router=require('./router');
 
-const server=http.createServer((req,res)=>{
+// const server=http.createServer((req,res)=>{
 
-    router(req,res);     
-});
-server.listen(3000,()=>{
-    console.log("Servet running at http://localhost:3000");
+//     router(req,res);     
+// });
+// server.listen(3000,()=>{
+//     console.log("Servet running at http://localhost:3000");
 
     
-})
+// })
+
+const http = require('http');
+const router = require('./router');
+const { connectDB } = require("./db");
+
+const server = http.createServer((req, res) => {
+    router(req, res);
+});
+
+connectDB().then(() => {
+    server.listen(3000, () => {
+        console.log("Server running at http://localhost:3000");
+    });
+});
